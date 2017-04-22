dde2f011b26b51d13259f122eacd51c17db92f96
Santeri Horttanainen <santeri.horttanainen@gmail.com>
2017-04-22 22:15:56 +0300

    Trying out templates and sfinae.

--- cppext/split.h
+++ cppext/split.h
@@ -9,10 +9,10 @@ namespace cppext
 			-> decltype(auto)
         {
             return std::find_first_of(
-                begin,
-                end,
-                delim.cbegin(),
-                delim.cend(),
+				std::forward<Iter>(begin),
+				std::forward<Iter>(end),
+				std::cbegin(delim),
+				std::cend(delim),
                 [](auto a, auto b) { return a != b; }
             );
         }
@@ -22,15 +22,15 @@ namespace cppext
 			-> decltype(auto)
         {
             return std::find_first_of(
-                begin,
-                end,
-                delim.cbegin(),
-                delim.cend()
+                std::forward<Iter>(begin),
+				std::forward<Iter>(end),
+				std::cbegin(delim),
+				std::cend(delim)
             );
         }
 
-        template <typename Iter, typename StringT, class Container>
-        auto recursiveSplit(Iter&& begin, Iter&& end, StringT&& delim, Container&& store)
+        template <typename Iter1, typename Iter2, typename StringT, class Container>
+        auto recursiveSplit(Iter1&& begin, Iter2&& end, StringT&& delim, Container& store)
 			-> void
         {
             auto beginCopy = findFirstNotOf(begin, end, delim);
@@ -38,16 +38,43 @@ namespace cppext
             {
                 auto endCopy = findFirstOf(beginCopy, end, delim);
                 store.emplace_back( beginCopy, endCopy );
-                recursiveSplit(endCopy, end, delim, store);
+                recursiveSplit(endCopy, std::forward<Iter2>(end), std::forward<StringT>(delim), store);
             }
         }
 
-		template<typename StringT, typename Container = std::vector<StringT>>
-		auto split(StringT&& toSplit, StringT&& delim)
-			->std::enable_if_t< std::is_same_v<StringT, typename Container::value_type>, Container>
+		template<typename Container = std::vector<std::string>>
+		auto split(const char* toSplit, const char* delim)
+		{
+			return split<std::string&&, std::string&&, Container>(std::string{ toSplit }, std::string{ delim });
+		}
+
+		template<typename StringT,typename Container = std::vector<std::string>>
+		auto split(StringT&& toSplit, const char* delim)
+		{
+			return split<StringT, std::string&&, Container>(std::forward<StringT>(toSplit), std::string{ delim });
+		}
+
+		template<typename StringT, typename Container = std::vector<std::string>>
+		auto split(const char* toSplit, StringT&& delim)
+		{
+			return split<std::string&&, StringT, Container>(std::string{ toSplit }, std::forward<StringT>(delim));
+		}
+
+		template<typename StringT1, typename StringT2, typename Container = std::vector<std::remove_reference_t<StringT1>>>
+		auto split(StringT1&& toSplit, StringT2&& delim)
+			->std::enable_if_t< std::is_same_v<std::remove_reference_t<StringT2>, char>, Container>
         {
-			auto store = Container{};
-            recursiveSplit(toSplit.cbegin(), toSplit.cend(), delim, store);
-            return store;
+			return split<StringT1, std::add_rvalue_reference_t<std::remove_reference_t<StringT1>>, Container>(std::forward<StringT1>(toSplit), std::remove_reference_t<StringT1>{delim});
         }
+
+		template<typename StringT1, typename StringT2, typename Container = std::vector<std::remove_reference_t<StringT1>>>
+		auto split(StringT1&& toSplit, StringT2&& delim)
+			->std::enable_if_t< ! std::is_same_v<std::remove_reference_t<StringT2>, char>, Container>
+		{
+			static_assert(std::is_same_v<std::remove_reference_t<StringT1>, typename Container::value_type>, "Container value type should be same as first of the split arguments.");
+
+			auto store = Container{};
+			recursiveSplit(std::cbegin(toSplit), std::cend(toSplit), std::forward<StringT2>(delim), store);
+			return store;
+		}
 }
--- test/split-test.cpp
+++ test/split-test.cpp
@@ -4,18 +4,21 @@
 #include "cppext/split.h"
 
 #include <type_traits>
+
+#include <vector>
 #include <list>
 
 using namespace cppext;
 using namespace std;
+using namespace testing;
 
 template<typename T>
-struct is_container : false_type
+struct is_vector : false_type
 {
 };
 
 template <typename T, typename Alloc>
-struct is_container<std::vector<T, Alloc>> : true_type
+struct is_vector<std::vector<T, Alloc>> : true_type
 {
 };
 
@@ -23,7 +26,7 @@ TEST(Split, DefaultReturnTypeIsVector)
 {
 	auto returnType = split(string{"a,b,c"}, string{ "," });
 
-	EXPECT_TRUE(is_container<decltype(returnType)>());
+	EXPECT_TRUE(is_vector<decltype(returnType)>());
 }
 
 //TEST(Split, DoesNotAllowContainersWithDifferentValueType)
@@ -31,4 +34,131 @@ TEST(Split, DefaultReturnTypeIsVector)
 //	split<string, vector<const char*>>(string{ "a,b,c" }, string{ "," });
 //}
 
-//// check that container value type is same as stringT
+TEST(Split, SplittingRValueStringWithRValueCharIsSupported)
+{
+	auto splitted = split(string{ "a,b,c,d,e" }, ',');
+
+	EXPECT_THAT(splitted.size(), Eq(5));
+}
+
+TEST(Split, SplittingLValueStringWithRValueCharIsSupported)
+{
+	auto toSplit = string{ "a,b,c,d,e" };
+
+	auto splitted = split(toSplit, ',');
+
+	EXPECT_THAT(splitted.size(), Eq(5));
+}
+
+TEST(Split, SplittingRValueStringWithLValueCharIsSupported)
+{
+	char delim = ',';
+
+	auto splitted = split(string{ "a,b,c,d,e" }, delim);
+
+	EXPECT_THAT(splitted.size(), Eq(5));
+}
+
+TEST(Split, SplittingLValueStringWithLValueCharIsSupported)
+{
+	auto toSplit = string{ "a,b,c,d,e" };
+	char delim = ',';
+
+	auto splitted = split(toSplit, delim);
+
+	EXPECT_THAT(splitted.size(), Eq(5));
+}
+
+TEST(Split, SplittingRValueStringWithRValueStringIsSupported)
+{
+	auto splitted = split(string{ "a,b,c,d,e" }, string{ "," });
+
+	EXPECT_THAT(splitted.size(), Eq(5));
+}
+
+TEST(Split, SplittingRValueStringWithLValueStringIsSupported)
+{
+	auto delim = string{","};
+
+	auto splitted = split(string{ "a,b,c,d,e" }, delim);
+
+	EXPECT_THAT(splitted.size(), Eq(5));
+}
+
+TEST(Split, SplittingLValueStringWithRValueStringIsSupported)
+{
+	auto toSplit = string{ "a,b,c,d,e" };
+
+	auto splitted = split(toSplit, string{ "," });
+
+	EXPECT_THAT(splitted.size(), Eq(5));
+}
+
+TEST(Split, SplittingLValueStringWithLValueStringIsSupported)
+{
+	auto toSplit = string{ "a,b,c,d,e" };
+	auto delim = string{ "," };
+
+	auto splitted = split(toSplit, delim);
+
+	EXPECT_THAT(splitted.size(), Eq(5));
+}
+
+TEST(Split, SplittingConstCharWithConstCharIsSupported)
+{
+	auto splitted = split("a,b,c,d,e", ",");
+
+	EXPECT_THAT(splitted.size(), Eq(5));
+}
+
+TEST(Split, SplittingRValueStringWithConstCharIsSupported)
+{
+	auto splitted = split(string{ "a,b,c,d,e" }, ",");
+
+	EXPECT_THAT(splitted.size(), Eq(5));
+}
+
+TEST(Split, SplittingLValueStringWithConstCharIsSupported)
+{
+	auto toSplit = string{ "a,b,c,d,e" };
+	auto splitted = split(toSplit, ",");
+
+	EXPECT_THAT(splitted.size(), Eq(5));
+}
+
+TEST(Split, SplittingConstCharWithRValueStringIsSupported)
+{
+	auto splitted = split("a,b,c,d,e", string{ "," });
+
+	EXPECT_THAT(splitted.size(), Eq(5));
+}
+
+TEST(Split, SplittingConstCharWithLValueStringIsSupported)
+{
+	auto delim = string{ "," };
+	auto splitted = split("a,b,c,d,e", delim);
+
+	EXPECT_THAT(splitted.size(), Eq(5));
+}
+
+TEST(Split, SplittingConstCharWithRValueCharIsSupported)
+{
+	auto splitted = split("a,b,c,d,e", ',');
+
+	EXPECT_THAT(splitted.size(), Eq(5));
+}
+
+TEST(Split, SplittingConstCharWithLValueCharIsSupported)
+{
+	auto delim = ',';
+	auto splitted = split("a,b,c,d,e", delim);
+
+	EXPECT_THAT(splitted.size(), Eq(5));
+}
+//
+//TEST(Split, ListCanBeUsedAsReturnedContainerType)
+//{
+//	auto splitted = split<const char*, const char*, list<string>>("a, b, c, d, e", ",");
+//
+//	EXPECT_THAT(splitted.size(), Eq(5));
+//}
